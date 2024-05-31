5e83e46b456766e31900237bff439d3dd72bf55e
Kalana Rubasinghe <kalanarajika99@gmail.com>
2024-05-31 12:02:52 +0530

    api url update

--- scrapper_fe/src/config/axiosConfig.js
+++ scrapper_fe/src/config/axiosConfig.js
@@ -1,6 +1,8 @@
 export default {
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: "https://web-scrapping-aolr.onrender.com",
     headers: {
         "content-type": "multipart/form-data"
     },
 };
+
+// baseURL: "http://127.0.0.1:8000",
