c41d80ea637a7bc2f67c1dad01aa9242fae79e5d
Kalana Rubasinghe <kalanarajika99@gmail.com>
2024-05-31 16:59:15 +0530

    FE styling init

--- scrapper_fe/src/App.jsx
+++ scrapper_fe/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import AppBarComp from './components/AppBarComp';
 import Homepage from './pages/Homepage';
 import WebContentComparison from './components/WebContentComparison';
 import NewsScrap from './components/NewsScrap';
@@ -8,14 +9,17 @@ import TextComparison from './components/TextComparison';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route exact path="/" element={<Homepage/>} />
-                <Route path="/website-comparison" element={<WebContentComparison/>} />
-                <Route path="/news-scraping" element={<NewsScrap/>} />
-                <Route path="/text-comparison" element={<TextComparison/>} />
-            </Routes>
-        </Router>
+        <>
+            <Router>
+                <AppBarComp />
+                <Routes>
+                    <Route exact path="/" element={<Homepage />} />
+                    <Route path="/website-comparison" element={<WebContentComparison />} />
+                    <Route path="/news-scraping" element={<NewsScrap />} />
+                    <Route path="/text-comparison" element={<TextComparison />} />
+                </Routes>
+            </Router>
+        </>
     );
 }
 
--- scrapper_fe/src/pages/Homepage.jsx
+++ scrapper_fe/src/pages/Homepage.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Typography, Container, List, ListItem, ListItemText, Button } from '@mui/material';
 
 function Homepage() {
     return (
-        <div>
-            <h1>Web Tools</h1>
-            <nav>
-                <ul>
-                    <li><Link to="/website-comparison">Website Content Comparison</Link></li>
-                    <li><Link to="/news-scraping">News Scraping</Link></li>
-                    <li><Link to="/text-comparison">Text Comparison</Link></li>
-                </ul>
-            </nav>
+        <div className='container container-fluid mx-auto'>
+            <Container maxWidth="md" sx={{ marginTop: 4 }}>
+                <Typography variant="h4" gutterBottom>
+                    Welcome to Web Tools
+                </Typography>
+                <List sx={{ mt: 2 }}>
+                    <ListItem sx={{ mb: 2 }}>
+                        <Button variant="outlined" component={Link} to="/website-comparison" fullWidth>
+                            <ListItemText primary="Website Content Comparison" />
+                        </Button>
+                    </ListItem>
+                    <ListItem sx={{ mb: 2 }}>
+                        <Button variant="outlined" component={Link} to="/news-scraping" fullWidth>
+                            <ListItemText primary="News Scraping" />
+                        </Button>
+                    </ListItem>
+                    <ListItem>
+                        <Button variant="outlined" component={Link} to="/text-comparison" fullWidth>
+                            <ListItemText primary="Text Comparison" />
+                        </Button>
+                    </ListItem>
+                </List>
+            </Container>
         </div>
     );
 }
