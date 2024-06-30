fbe290882612363fb594d945e880b5ccb5e67497
Kalana Rubasinghe <kalanarajika99@gmail.com>
2024-07-01 00:13:10 +0530

    Update App.jsx

--- scrapper_fe/src/App.jsx
+++ scrapper_fe/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
             const hours = now.getHours();
             const minutes = now.getMinutes();
 
-            if (hours === 9 && minutes === 15) {
+            if (hours === 9 && minutes === 0) {
                 callAPI();
             }
         };
